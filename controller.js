bba4c73f85317c1dcc46513aeed26f2802ec08ce
JacobJSLove <kang2@o2.pl>
2017-04-09 01:57:52 +0200

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,10 +1,17 @@
-// Method chaining
-var app = angular.module('app', [])
-				 .controller('myCtrl', function ($scope){
-					$scope.oferty = [
-					{ nazwa: 'Obiekt 1', cena: 150 },
-					{ nazwa: 'Obiekt 2', cena: 100 },
-					{ nazwa: 'Obiekt 3', cena: 250 },
-					];
-});
-				 
+var app = angular.module('app', []);			 
+app.controller('1Ctrl', function ($scope){
+		var technologies = [
+		{name:"HTML", like:0, dislike:0},
+		{name:"CSS", like:0, dislike:0},
+		{name:"JQ", like:0, dislike:0},
+		{name:"SQL", like:0, dislike:0},
+		];
+		$scope.technologies = technologies;
+
+$scope.addlike = function(technology) {
+	technology.like++;
+};
+$scope.adddislike = function(technology) {
+	technology.dislike++;
+};
+});		 
