30e4ff6f4448183c57259add35570c44b5f65759
JacobJSLove <kang2@o2.pl>
2017-04-09 02:29:40 +0200

    Add files via upload

--- angular.php
+++ angular.php
@@ -9,11 +9,34 @@
 </style>
 </head>
 <body>
-	<div ng-controller="dateCtrl">
-	Data to : {{ orginal() | date }} 
-	</div>
-		<div ng-controller="NewWords">
-	Zdanie to  : {{ funkcjaA() }} 
+	<div ng-controller="Ctrl">
+		Order By : <select ng-model="sortCol">
+					<option  value="name">Name ASC</option>
+					<option selected value="gender">Gender ASC</option>
+					<option value="dateOfBirth">DateOfBirth ASC</option>
+					<option value="-salary">Salary DESC</option>
+
+					</select>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Date</th>
+					<th>Gender</th>
+					<th>Salary</th>
+					<th>Salary</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr ng-repeat="employee in employees | orderBy:sortCol">
+					<td>{{employee.name | uppercase}}</td>
+					<td>{{employee.dateOfBirth | date:"dd/MM/yyyy"}}</td>
+					<td>{{employee.gender | lowercase}}</td>
+					<td>{{employee.salary | number:2}}</td>
+					<td>{{employee.salary | currency:"&":1}}</td>
+				</tr>
+			</tbody>
+		</table>	
 	</div>
 </body>
 <script src="jquery-3.1.0.min.js"></script>
