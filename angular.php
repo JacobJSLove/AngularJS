47cd113c36e947f450e447118ba56dcbc4694717
JacobJSLove <kang2@o2.pl>
2017-04-18 03:01:44 +0200

    Add files via upload

--- angular.php
+++ angular.php
@@ -10,37 +10,25 @@
 </head>
 <body>
 	<div ng-controller="Ctrl">
-		Search : <input type="text" placeholder="Search employee" ng-model="searchText.name"/>
-		<table class="table">
-			<thead>
-				<tr>
-					<th>
-						Name 
-					</th>
-					<th>
-						Date 
-					</th>
-					<th>
-						Gender 
-					</th>
-					<th>
-						Salary 
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr ng-repeat="employee in employees | filter:searchText">
-					<td>{{employee.name | uppercase}}</td>
-					<td>{{employee.dateOfBirth | date:"dd/MM/yyyy"}}</td>
-					<td>{{employee.gender | lowercase}}</td>
-					<td>{{employee.salary | number:2}}</td>
-				</tr>
-			</tbody>
-		</table>	
+        <table class="table">
+                <tr>
+                    <td>String</td>
+                    <td> <input type="text" ng-model="input"/> </td>
+                </tr>
+                <tr>
+                    <td>Result</td>
+                    <td> <input type="text" ng-model="output"/> </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td> <input type="button" ng-click="tranform(input)" value="Change String" /> </td>
+                </tr>
+        </table>
 	</div>
 </body>
 <script src="jquery-3.1.0.min.js"></script>
 <script src="angular.js"></script>
 <script src="controller.js"></script>
+<script src="stringService.js"></script>
 <script src="bootstrap.js"></script>
 </html>
