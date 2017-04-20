144f13ef86d213f4818ad988c5a8dfb890c3db4b
JacobJSLove <kang2@o2.pl>
2017-04-20 03:24:38 +0200

    Add files via upload

--- angular.php
+++ angular.php
@@ -10,25 +10,33 @@
 </head>
 <body>
 	<div ng-controller="Ctrl">
-        <table class="table">
-                <tr>
-                    <td>String</td>
-                    <td> <input type="text" ng-model="input"/> </td>
-                </tr>
-                <tr>
-                    <td>Result</td>
-                    <td> <input type="text" ng-model="output"/> </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td> <input type="button" ng-click="tranform(input)" value="Change String" /> </td>
-                </tr>
-        </table>
+<button id="top" ng-click="scroll('bottom')">
+        Go to bottom of the page
+</button>
+    <br /><br />
+    <div>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sodales, turpis a hendrerit scelerisque, lectus nunc bibendum justo, a ornare erat erat ac dui. Vivamus semper in sem at accumsan. Integer rutrum vehicula lacus, ac lobortis dolor sodales in. Cras eleifend augue at nunc feugiat vulputate. Donec enim dui, consectetur eget justo sit amet, gravida scelerisque tellus. Sed imperdiet turpis tincidunt lectus condimentum, a pulvinar quam porttitor. Aliquam erat volutpat. Aenean eget placerat lectus. Vivamus at dolor mattis, suscipit arcu condimentum, rutrum risus. Aenean accumsan purus dolor, ac pellentesque erat luctus vel. Praesent eget tincidunt mauris. Morbi nulla libero, placerat nec tellus at, lobortis porta sem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi dignissim vel diam in varius. Morbi quis tristique eros, id rhoncus quam. Duis vitae ultrices purus.
+
+Maecenas eu elit sed nunc condimentum dictum egestas quis metus. Duis nec finibus mauris, ut maximus tortor. Sed mollis arcu a laoreet interdum. Duis iaculis fermentum sem id faucibus. Praesent vestibulum purus vitae nisi varius ullamcorper. Ut sit amet feugiat mauris, sit amet accumsan nisi. Sed sed quam rutrum, feugiat tellus et, congue neque. Nullam eget libero dui. Nulla convallis neque arcu, sit amet condimentum magna lacinia sed. Donec efficitur quam id nisl posuere, quis sodales urna consectetur.
+
+Nullam consequat arcu vitae nunc ultricies, nec tempus lorem lobortis. Mauris eu eros placerat, maximus arcu id, tincidunt urna. Sed porta consequat faucibus. Nulla vel ex rutrum, sagittis nisi non, varius enim. Praesent lacus est, faucibus nec ornare id, rutrum sed augue. Aenean in volutpat velit, sed tempus tortor. Integer blandit quam nunc, vel luctus lectus porta nec. Vivamus euismod, lacus non sollicitudin eleifend, enim nulla dignissim elit, ut pellentesque lectus metus et justo. Fusce consequat, lectus in ornare rhoncus, augue leo mollis massa, nec molestie turpis magna sed ligula. Vestibulum blandit maximus mi. Morbi turpis neque, suscipit et consectetur vel, placerat et ipsum. Quisque finibus finibus ipsum, vitae dictum augue blandit eget. Aliquam felis metus, laoreet in nunc ac, egestas fermentum dolor. Nulla lacinia vitae dolor sed condimentum. Pellentesque rutrum nulla eget sapien tristique vehicula. Quisque nec ligula sit amet odio luctus semper.
+
+Morbi vulputate, tellus id varius finibus, massa mi venenatis ipsum, bibendum efficitur turpis nibh at tellus. Sed eleifend mi quis cursus semper. Fusce molestie erat eget scelerisque rhoncus. Vivamus eu interdum mauris, vitae suscipit arcu. In hac habitasse platea dictumst. Pellentesque semper hendrerit lacinia. Nullam mollis erat sed erat dictum, eget aliquet arcu ullamcorper. Curabitur maximus non sem eget posuere. Sed ut ante eget tellus consequat tincidunt eget non ligula. Donec fermentum leo sed tincidunt malesuada. Donec a rutrum ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent elit lorem, vulputate ac pellentesque sed, faucibus a sapien. Ut sit amet sapien magna.
+
+Donec elementum mi ut neque tincidunt lacinia. Maecenas porta tortor vel erat tincidunt, sagittis dapibus leo fermentum. Suspendisse eget erat vehicula, hendrerit tellus ac, imperdiet nulla. Ut ipsum dolor, fermentum ac arcu in, eleifend ornare lacus. In sodales sollicitudin velit et vehicula. Phasellus in blandit mauris. Praesent quis fringilla urna. Vivamus convallis feugiat elit sed efficitur. Maecenas varius sed purus et placerat. Pellentesque fermentum accumsan sodales. Integer egestas auctor augue, non tempor ligula sollicitudin eget. Aliquam erat volutpat. Proin dapibus lectus justo, sit amet mollis metus auctor quis. Proin vel vehicula ligula.
+
+Aliquam quis efficitur nisi. Maecenas varius orci non nibh malesuada, in ultricies ligula aliquam. Curabitur maximus, elit sed congue ullamcorper, ex massa laoreet quam, vitae maximus erat ex sed ante. In interdum dolor magna, luctus ultricies mi hendrerit a. Sed venenatis vehicula euismod. Nullam dapibus erat eget nisl commodo blandit. Praesent cursus condimentum lacus fermentum semper. Pellentesque volutpat congue ante sed pulvinar. Ut sed tristique lacus, suscipit venenatis nibh. Nulla tempor tempus leo vitae porttitor. Sed convallis velit in nisi interdum pulvinar.
+
+Donec aliquet ligula mi, non fermentum lacus lacinia sed. In eget nulla ac justo vulputate cursus nec non mi. Aenean iaculis sed ex sed aliquet. Maecenas sagittis nisi eu augue finibus placerat. Pellentesque quis nisl egestas, maximus neque sit amet, tempor magna. Sed semper gravida tortor sit amet vehicula. Vestibulum et neque quis nunc sodales viverra in quis nulla. Suspendisse rutrum magna tempus mattis mattis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam vitae elit fringilla, pellentesque libero vel, ornare libero. Aenean tristique, lectus et dignissim dictum, massa nulla consectetur risus, ac ullamcorper leo elit at diam. Nulla pulvinar urna ac congue ornare. Nunc non elit congue, tincidunt arcu id, pulvinar velit.
+    </div>
+    <br />
+    <button id="bottom" ng-click="scroll('top')">
+        Go to top of the page
+    </button>
 	</div>
 </body>
 <script src="jquery-3.1.0.min.js"></script>
 <script src="angular.js"></script>
 <script src="controller.js"></script>
-<script src="stringService.js"></script>
 <script src="bootstrap.js"></script>
 </html>
