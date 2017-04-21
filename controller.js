afc3f5f9a6dbcfc437a731cc5f9b0154dcf225db
JacobJSLove <kang2@o2.pl>
2017-04-21 02:59:59 +0200

    Add files via upload

--- controller.js
+++ controller.js
@@ -1,30 +1,15 @@
-var app = angular.module('app', []);			 
-app.controller('Ctrl', function ($scope){
-	   var employees = [
-                {
-                    name: "Ben", dateOfBirth: new Date("November 23, 1980"),
-                    gender: "Male", salary: 55000
-                },
-                {
-                    name: "Sara", dateOfBirth: new Date("May 05, 1970"),
-                    gender: "Female", salary: 68000
-                },
-                {
-                    name: "Mark", dateOfBirth: new Date("August 15, 1974"),
-                    gender: "Male", salary: 57000
-                },
-                {
-                    name: "Pam", dateOfBirth: new Date("October 27, 1979"),
-                    gender: "Female", salary: 53000
-                },
-                {
-                    name: "Todd", dateOfBirth: new Date("December 30, 1983"),
-                    gender: "Male", salary: 60000
-                }
-            ];
+var app = angular.module('app', []);	
+                     .controller("countryController",
+                        function ($scope, $location, $anchorScroll, $http) {
+                         $http.get("CountryService.asmx/GetData")
+                              .then(function (response) {
+                                  $scope.countries = response.data;
+                              });
 
-            $scope.employees = employees;
+                         $scope.scrollTo = function (countryName) {
+                             $location.hash(countryName);
+                             $anchorScroll();
+                         }
 
-});
-
+                     });
 
